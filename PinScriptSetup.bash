ced01bc79f0d8e00dcfd29b389b37655de058d10
techcoderx <techcoderx@TechCoderXs-Mac-Pro.local>
2018-09-09 15:53:43 +0800

    Add setup .bash script for .sh pinning scripts

---
+++ PinScriptSetup.bash
@@ -0,0 +1,6 @@
+chmod a+rx DTubePinFiles.sh
+chmod a+rx DSoundPinFiles.sh
+chmod a+rx PinFiles.sh
+chmod a+rx cleanup.sh
+chmod a+rx Pinned/rmPins.sh
+echo 'You may now execute IPFS pinning scripts by typing ./PinFiles.sh.'
--- Pinned/rmPins.sh
+++ Pinned/rmPins.sh
@@ -5,3 +5,4 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
         ipfs pin rm $line
     fi
 done < $1.txt
+rm $1.txt
